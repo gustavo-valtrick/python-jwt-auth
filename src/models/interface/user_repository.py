a11cb98ece22880cbb4bbf8a12aef9c599d99164
gustavo-valtrick <guvaltrick@outlook.com>
2026-07-22 15:14:33 -0300

    feat: implement interface to repository

---
+++ src/models/interface/user_repository.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+class UserRepositoryInterface(ABC):
+
+    @abstractmethod
+    def registry_user(self, username: str, password: str) -> None: ...
+
+    @abstractmethod
+    def edit_balance(self, user_id: int, new_balance: float) -> None: ...
+
+    @abstractmethod
+    def get_user_by_username(self, username: str) -> tuple[int, str, str]: ...
